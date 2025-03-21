c40bc1825e6c6a2ac9c1c1af51dc5b8ac2a4cdfc
markscat <markscat@gmail.com>
2025-03-21 13:34:56 +0800

    第一版本
    IDE：Visual Studio 2022　
    環境：Win11
    主要功能：
    1.　選擇通訊埠和鮑率
    2.　開啟通訊埠
    就這樣。

---
+++ ComPort_Charp/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace ComPort_Charp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BuConnect = new System.Windows.Forms.Button();
+            this.BuClear = new System.Windows.Forms.Button();
+            this.comport = new System.Windows.Forms.ComboBox();
+            this.board_rat = new System.Windows.Forms.ComboBox();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.SuspendLayout();
+            // 
+            // BuConnect
+            // 
+            this.BuConnect.Location = new System.Drawing.Point(632, 39);
+            this.BuConnect.Name = "BuConnect";
+            this.BuConnect.Size = new System.Drawing.Size(75, 23);
+            this.BuConnect.TabIndex = 0;
+            this.BuConnect.Text = "Connect";
+            this.BuConnect.UseVisualStyleBackColor = true;
+            this.BuConnect.Click += new System.EventHandler(this.BuConnect_Click);
+            // 
+            // BuClear
+            // 
+            this.BuClear.Location = new System.Drawing.Point(722, 39);
+            this.BuClear.Name = "BuClear";
+            this.BuClear.Size = new System.Drawing.Size(75, 22);
+            this.BuClear.TabIndex = 1;
+            this.BuClear.Text = "clear";
+            this.BuClear.UseVisualStyleBackColor = true;
+            // 
+            // comport
+            // 
+            this.comport.FormattingEnabled = true;
+            this.comport.Location = new System.Drawing.Point(38, 39);
+            this.comport.Name = "comport";
+            this.comport.Size = new System.Drawing.Size(121, 23);
+            this.comport.TabIndex = 2;
+            // 
+            // board_rat
+            // 
+            this.board_rat.FormattingEnabled = true;
+            this.board_rat.IntegralHeight = false;
+            this.board_rat.Location = new System.Drawing.Point(235, 39);
+            this.board_rat.Name = "board_rat";
+            this.board_rat.Size = new System.Drawing.Size(109, 23);
+            this.board_rat.TabIndex = 3;
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Location = new System.Drawing.Point(24, 68);
+            this.textBoxOutput.Multiline = true;
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.Size = new System.Drawing.Size(874, 409);
+            this.textBoxOutput.TabIndex = 4;
+            this.textBoxOutput.TextChanged += new System.EventHandler(this.textBoxOutput_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Selected COM Port";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(232, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Selected BaudRate";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(910, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(910, 489);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.board_rat);
+            this.Controls.Add(this.comport);
+            this.Controls.Add(this.BuClear);
+            this.Controls.Add(this.BuConnect);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BuConnect;
+        private System.Windows.Forms.Button BuClear;
+        private System.Windows.Forms.ComboBox comport;
+        private System.Windows.Forms.ComboBox board_rat;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+    }
+}
+
---
+++ ComPort_Charp/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComPort_Charp
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort _serialPort;
+        //private ComboBox cmbPorts;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "串口通訊工具"; // 設定視窗標題
+
+            _serialPort = new SerialPort();
+            board_rat.Items.AddRange(new object[] { 9600, 19200, 38400, 57600, 115200, 256000 });
+            board_rat.SelectedIndex = 0;
+
+
+
+            string[] ports = SerialPort.GetPortNames();  // 獲取可用的 COM 端口
+            if (ports.Length == 0)
+            {
+                MessageBox.Show("未偵測到任何可用的 COM 端口！請檢查設備連接。", "警告", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            
+
+            // 在 ComboBox 中填充所有串口名稱
+            //cmbPorts.Items.AddRange(ports);
+            comport.Items.AddRange(SerialPort.GetPortNames());
+            if (comport.Items.Count > 0)
+                comport.SelectedIndex = 0;  // 預設選擇第一個端口
+
+            //Controls.Add(textBoxOutput);  // 添加到 Form 控件中
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 在ComboBox中填充所有串口名稱
+            comport.Items.AddRange(SerialPort.GetPortNames());
+            
+
+
+        }
+
+        private void BuConnect_Click(object sender, EventArgs e)
+        {
+            if (_serialPort.IsOpen) // 如果串口已開啟，則執行關閉操作
+            {
+                try
+                {
+                    _serialPort.Close();
+                    BuConnect.Text = "Connect"; // 按鈕顯示「Connect」
+                    MessageBox.Show("串口已關閉！", "成功", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("無法關閉串口：" + ex.Message, "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else // 如果串口未開啟，則執行開啟操作
+            {
+                if (comport.SelectedItem == null)
+                {
+                    MessageBox.Show("請先選擇一個 COM 端口！", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                if (board_rat.SelectedItem == null)
+                {
+                    MessageBox.Show("請選擇鮑率！", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                string selectedPort = comport.SelectedItem.ToString();
+                int selectedBaudRate = Convert.ToInt32(board_rat.SelectedItem);
+
+                try
+                {
+                    _serialPort.PortName = selectedPort;
+                    _serialPort.BaudRate = selectedBaudRate;
+                    _serialPort.Open();
+                    _serialPort.DataReceived += SerialPort_DataReceived;
+                    BuConnect.Text = "Disconnect"; // 按鈕顯示「Disconnect」
+                    MessageBox.Show("串口已成功開啟！", "成功", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("無法開啟串口：" + ex.Message, "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+#if BuConnect_Click_one
+        private void BuConnect_Click(object sender, EventArgs e)
+        {
+            if (comport.SelectedItem == null)
+            {
+                MessageBox.Show("請先選擇一個 COM 端口！", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (board_rat.SelectedItem == null)
+            {
+                MessageBox.Show("請選擇鮑率！", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string selectedPort = comport.SelectedItem.ToString();
+            int selectedBaudRate = Convert.ToInt32(board_rat.SelectedItem);
+
+            MessageBox.Show("嘗試開啟端口：" + selectedPort, "資訊", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            try
+            {
+                _serialPort.PortName = selectedPort;
+                _serialPort.BaudRate = selectedBaudRate;
+                _serialPort.Open();
+                _serialPort.DataReceived += SerialPort_DataReceived;
+                MessageBox.Show("串口已成功開啟！", "成功", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("無法開啟串口：" + ex.Message, "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+#endif
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            // 接收到數據後顯示
+            string data = _serialPort.ReadExisting();
+            Invoke(new Action(() => textBoxOutput.AppendText(data)));
+        }
+        private void btnClosePort_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxOutput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
